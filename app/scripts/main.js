4c98eeeb1d71be0eb454c9b92da419924b930cf7
Munaf Assaf <munaf.assaf@gmail.com>
2013-10-28 21:41:38 -0700

    Add Markov Library

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,12 +1,14 @@
 require.config({
     paths: {
-        jquery: '../bower_components/jquery/jquery'
+        jquery: '../bower_components/jquery/jquery',
+        markov: 'markov.min'
     }
 });
 
-require(['app', 'jquery'], function (app, $) {
+require(['app', 'jquery', 'markov'], function (app, $, Markov) {
     'use strict';
     // use app here
     console.log(app);
     console.log('Running jQuery %s', $().jquery);
+    console.log('Markov Lib: ', Markov);
 });
