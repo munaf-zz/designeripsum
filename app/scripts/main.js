b2e2a17f5f415bf6c46f667f1f5a0ff2e6d9e135
Munaf Assaf <munaf.assaf@gmail.com>
2013-11-07 21:18:02 -0800

    Basic functionality: displaying designer-specific output
    Successfully outputting text based on actual designer quotes.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,33 +1,73 @@
 require.config({
-    paths: {
-        jquery: '../bower_components/jquery/jquery',
-        markov: 'markov.min'
-    }
+  paths: {
+    jquery: '../bower_components/jquery/jquery',
+    markov: 'markov.min'
+  }
 });
 
-require(['app', 'jquery', 'markov'], function (app, $) {
-    'use strict';
+require(['app', 'jquery', 'markov'], function(app, $) {
+  'use strict';
 
-    var designerQuotes;
-    var markov = {}, designer;
+  var designers, json;
+  var $designerList, $output;
+
+  $designerList = $('.designer-list');
+  $output       = $('article');
+
+  json = getJson('scripts/designer-text.json');
+  designers = createMarkov(json);
+
+  $('.designer-link').on('click', function(event) {
+    var designer = $(this).attr('id');
+
+    $output.append('<p>' + designers[designer].generate(100) + '</p>');
+
+  });
+
+  function getJson(url) {
+    var designerJson = {};
 
     $.ajax({
-    	url: 'scripts/designer-text.json',
-    	dataType: 'json',
-    	async: false
+      url: url,
+      dataType: 'json',
+      async: false
     }).done(function(data) {
-    	console.log("got here!");
-    	designerQuotes = data;
+      designerJson = data;
     });
 
-    console.log("designer quotes here: ", designerQuotes);
+    return designerJson;
+  }
 
-    for (designer in designerQuotes) {
-    	markov[designer] = new Markov({
-    		inputText: designerQuotes[designer]['quotes'].join(' '),
-    		endsWithCompleteSentence: true
-    	});
+  function createMarkov(designerJson) {
+    var markov = {},
+        allDesignerQuotes = [],
+        quotes, designer;
+
+    // Seperate generators for each designer
+    for (designer in designerJson) {
+      quotes = designerJson[designer]['quotes'];
+
+      markov[designer] = new Markov({
+        inputText: quotes.join(' '),
+        endWithCompleteSentence: true
+      });
+
+      $designerList.append(
+        '<li><a href="#" class="designer-link" id="' + designer + '">' + designerJson[designer]['name'] + '</a></li>'
+      );
+
+      console.log($designerList);
+
+      allDesignerQuotes.concat(quotes);
     }
 
-    console.log(markov['edwardtufte'].generate(100));
+    // Mix of all designers
+    markov['all'] = new Markov({
+      inputText: allDesignerQuotes.join(' '),
+      endsWithCompleteSentence: true
+    }); 
+
+    return markov;
+  }
+
 });
