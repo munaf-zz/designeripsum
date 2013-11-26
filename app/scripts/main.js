c373a6a4887aa11d7235f3c4c77d4069345251ca
Munaf Assaf <munaf.assaf@gmail.com>
2013-11-25 17:58:41 -0800

    Final release styling ready

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -7,40 +7,12 @@ require.config({
 require(['app', 'jquery'], function(app, $) {
   'use strict';
 
-  var designerSelector = {list: {}, selected: 0};
-  var $output, $selection, $list, designers, randomWords, json, data;
-
-  $output = $('article');
-  $list = $('.designer-list');
-  $selection = $('.select-designer');
-
-  json = getJson('scripts/designer-text.json');
-  designers = createMarkov(json);
-
-  selectDesigner('all');
-  generateText();
-
-  $output.on('click', function(event) {
-    selectText('article');
-  });
-
-  $('.add-button').on('click', function(event) {
-    event.preventDefault();
-    deselectText();
-    generateText();
-    //$(window).scrollTop($(document).height());
-  });
-
-  $('.select-designer').on('click', function(event) {
-    event.preventDefault();
-
-    var name = $(this).attr('class').split(' ')[1];console.log( name);
-
-    selectDesigner(name);
-    $('.selected').removeClass('selected');
-    $(this).addClass('selected');
-    deselectText();
-  });
+  var $output, $selection, $list, designers, randomWords, json;
+  var Markov = window.Markov;
+  var designerSelector = {
+    list: {},
+    selected: 0
+  };
 
   function selectDesigner(name) {
     designerSelector.selected = name;
@@ -48,10 +20,15 @@ require(['app', 'jquery'], function(app, $) {
     $('.' + name).addClass('selected');
   }
 
+  function removeText() {
+    $output.find('p:first-of-type').remove();
+  }
+
   function generateText() {
     var numWords = 30 + Math.floor(Math.random() * 30),
-        selected = designerSelector.selected, 
-        newText; console.log(designers);
+      selected = designerSelector.selected,
+      newText;
+    console.log(designers);
 
     if (selected === 'buzzwords') {
       newText = '<p>' + randomParagraph(numWords) + '</p>';
@@ -60,7 +37,7 @@ require(['app', 'jquery'], function(app, $) {
     }
 
     $output.prepend(newText);
-    
+
   }
 
   function getJson(url) {
@@ -79,25 +56,25 @@ require(['app', 'jquery'], function(app, $) {
 
   function randomParagraph(numWords) {
     var sentenceLength,
-        paragraph = '';
-
-    while (numWords > 0) {
-      sentenceLength = Math.floor(Math.random()*2) + 2;
-      paragraph += addSentence(sentenceLength) + ' ';
-      numWords -= sentenceLength;
-    }
+      paragraph = '';
 
     function addSentence(words) {
       var sentence = '';
 
       for (var i = 0; i < words; i++) {
         sentence += randomWords[
-          Math.floor(Math.random()*randomWords.length)
+          Math.floor(Math.random() * randomWords.length)
         ];
         sentence += ' ';
       }
 
       return sentence.charAt(0).toUpperCase() + sentence.slice(1, -1) + '.';
+    }
+
+    while (numWords > 0) {
+      sentenceLength = Math.floor(Math.random() * 4) + 2;
+      paragraph += addSentence(sentenceLength) + ' ';
+      numWords -= sentenceLength + 1;
     }
 
     return paragraph;
@@ -111,11 +88,11 @@ require(['app', 'jquery'], function(app, $) {
     // Seperate generators for each designer
     for (designer in designerJson) {
       if (designer === 'random') {
-        designerSelector.list['buzzwords'] = {
+        designerSelector.list.buzzwords = {
           lastName: 'Buzzwords'
         };
 
-        randomWords = designerJson['random'].words;
+        randomWords = designerJson.random.words;
 
         continue;
       }
@@ -134,31 +111,32 @@ require(['app', 'jquery'], function(app, $) {
 
       $list.append([
         '<li>',
-          '<a href="#" class="select-designer ' + designer + '">',
-            designerJson[designer].name.split(' ')[1],
-          '</a>',
+        '<a href="#" class="select-designer ' + designer + '">',
+        designerJson[designer].name.split(' ')[1],
+        '</a>',
         '</li>'
       ].join(''));
     }
 
     // Mix of all designers
-    designerSelector.list['mix'] = {
+    designerSelector.list.mix = {
       lastName: 'Mix'
     };
 
-    markov['all'] = new Markov({
+    markov.all = new Markov({
       inputText: allDesignerQuotes.join(' '),
       endWithCompleteSentence: true
     });
 
-    $list.prepend('<li><a href="#" class="select-designer all">Mix</a></li>');
-    $list.append('<li><a href="#" class="select-designer buzzwords">Buzzwords</a></li>');
+    $('<li><a href="#" class="select-designer all">Mix</a></li>').insertAfter('.remove-button');
+    $list.append('<li><a href="#" class="select-designer buzzwords">Misc.</a></li>');
 
     return markov;
   }
 
   // Code adapted from: http://stackoverflow.com/a/987376
   // And from downtonipsum.com :-)
+
   function selectText(element) {
 
     var doc = document,
@@ -177,20 +155,85 @@ require(['app', 'jquery'], function(app, $) {
       selection.removeAllRanges();
       selection.addRange(range);
     }
-    
-  };
+
+  }
 
   // Code adapted from: http://help.dottoro.com/ljigixkc.php
   // And from downtonipsum.com :-)
+
   function deselectText() {
-    if (window.getSelection) {  // All browsers, except <= IE8
-      var selection = window.getSelection();                                        
+    $output.find('p:first-of-type').removeClass('insert-above');
+    $output.find('p:first-of-type').removeClass('selected-for-removal');
+
+    if (window.getSelection) { // All browsers, except <= IE8
+      var selection = window.getSelection();
       selection.removeAllRanges();
     } else {
       if (document.selection.createRange) { // <= IE8
-        var range = document.selection.createRange ();
-        document.selection.empty ();
+        var range = document.selection.createRange();
+        document.selection.empty();
       }
     }
-  };
+  }
+
+  $output = $('article');
+  $list = $('.nav-list');
+  $selection = $('.select-designer');
+
+  json = getJson('designer-text.json');
+  designers = createMarkov(json);
+
+  selectDesigner('all');
+  generateText();
+
+  $output.on('click', function(event) {
+    event.preventDefault();
+    selectText('article');
+  });
+
+  $('.add-button').on('mouseover', function(event) {
+    event.preventDefault();
+    deselectText();
+    $output.find('p:first-of-type').addClass('insert-above');
+  });
+
+  $('.add-button').on('mouseout', function(event) {
+    event.preventDefault();
+    deselectText();
+    $output.find('p:first-of-type').removeClass('insert-above');
+  });
+
+  $('.add-button').on('click', function(event) {
+    event.preventDefault();
+    deselectText();
+    generateText();
+  });
+
+  $('.remove-button').on('mouseover', function(event) {
+    event.preventDefault();
+    $output.find('p:first-of-type').addClass('selected-for-removal');
+  });
+
+  $('.remove-button').on('mouseout', function(event) {
+    event.preventDefault();
+    $output.find('p:first-of-type').removeClass('selected-for-removal');
+  });
+
+  $('.remove-button').on('click', function(event) {
+    event.preventDefault();
+    deselectText();
+    removeText();
+  });
+
+  $('.select-designer').on('click', function(event) {
+    event.preventDefault();
+
+    var name = $(this).attr('class').split(' ')[1];
+
+    selectDesigner(name);
+    $('.selected').removeClass('selected');
+    $(this).addClass('selected');
+    deselectText();
+  });
+
 });
